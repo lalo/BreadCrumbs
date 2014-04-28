d8888cd3608de42982389f9ab9f27cd3853601b2
Eduardo Salinas <eduardosalinas@hotmail.com>
2014-04-27 20:29:14 -0500

    zoom into user location added

--- Location Logger/HLLMapViewController.m
+++ Location Logger/HLLMapViewController.m
@@ -18,6 +18,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _mapView.delegate = self;
+    _mapView.showsUserLocation = YES;
+}
+
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion mapRegion;
+    mapRegion.center = mapView.userLocation.coordinate;
+    mapRegion.span.latitudeDelta = 0.2;
+    mapRegion.span.longitudeDelta = 0.2;
+    
+    [mapView setRegion:mapRegion animated: YES];
 }
 
 - (void)didReceiveMemoryWarning
